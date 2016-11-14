fdd21e0c83d8d83ac4c5fd8b051c0bf1914a9a2c
Marc-Velay <marc.velay@gmail.com>
2016-11-14 16:54:56 +0100

    added new intent, maps, buttons, textviews

--- app/src/main/java/com/esiea/chaffre_velay/cvprojects/MainActivity.java
+++ app/src/main/java/com/esiea/chaffre_velay/cvprojects/MainActivity.java
@@ -1,13 +1,90 @@
 package com.esiea.chaffre_velay.cvprojects;
 
+import android.app.DatePickerDialog;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.NotificationCompat;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView txt;
+    DatePickerDialog dpd;
+    NotificationManager notMan;
+    NotificationCompat.Builder builder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        txt = (TextView)findViewById(R.id.textBox);
+        DatePickerDialog.OnDateSetListener odsl = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int i, int i1, int i2) {
+                SimpleDateFormat dateFormatter = new SimpleDateFormat("MMM dd, yyyy");
+                Date date = new Date(i-1900, i1, i2);
+                txt.setText(dateFormatter.format(date));
+            }
+        };
+        dpd = new DatePickerDialog(this, odsl, Calendar.getInstance().get(Calendar.YEAR), Calendar.getInstance().get(Calendar.MONTH), Calendar.getInstance().get(Calendar.DAY_OF_MONTH));
+
+        builder = new NotificationCompat.Builder(this);
+        builder.setSmallIcon(R.mipmap.ic_launcher);
+        builder.setContentText("Title? nop content");
+        builder.setContentTitle("oooh this is the title");
+        notMan = (NotificationManager)getSystemService(NOTIFICATION_SERVICE);
+    }
+
+    public void doBtn(View v) {
+        txt.setText("button clicked!");
+        dpd.show();
+    }
+
+    public void doBtnBack(View v) {
+        txt.setText(R.string.textBoxTxt);
+        Toast.makeText(getApplicationContext(), getString(R.string.msg), Toast.LENGTH_SHORT).show();
+        notMan.notify(500, builder.build());
+    }
+
+    public void openMap(View v) {
+        startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("geo:0,0?q=Londre")));
+    }
+
+    public void changeIntent(View v) {
+        Intent intent = new Intent(this, secondeActivity.class);
+        startActivity(intent);
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.toast_me) {
+            Toast.makeText(getApplicationContext(), getString(R.string.roastme), Toast.LENGTH_SHORT).show();
+        } else if (id == R.id.toast_that) {
+            Toast.makeText(getApplicationContext(), getString(R.string.roastThat), Toast.LENGTH_SHORT).show();
+        } if (id == R.id.toast_bread) {
+            Toast.makeText(getApplicationContext(), getString(R.string.roastBread), Toast.LENGTH_SHORT).show();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
---
+++ app/src/main/java/com/esiea/chaffre_velay/cvprojects/secondeActivity.java
@@ -0,0 +1,13 @@
+package com.esiea.chaffre_velay.cvprojects;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class secondeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_seconde);
+    }
+}
